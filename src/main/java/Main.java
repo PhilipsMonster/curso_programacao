403e452c7d2518dbce870944266c67066b9b933a
Marcelo Gomes <philipsmonster@gmail.com>
2020-10-24 06:55:49 -0300

    Atualizando no git

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class Main {
         public static void main(String[] args) {
-            System.out.println("Olá mundo!");
+
+            Scanner sc = new Scanner((System.in));
+
+            System.out.println("Enter three numbers");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            int higher = max(a, b, c);
+
+            showResult(higher);
+
+            sc.close();
+
         }
+
+        public static int max(int a, int b, int c){
+
+            int aux = 0;
+
+            if (a > b && a > c){
+                aux = a;
+            }
+            else if (b > c){
+                aux = b;
+            }
+            else {
+                aux = c;
+            }
+
+            return aux;
+        }
+
+        public static void showResult(int higher){
+
+            System.out.println("Higher " + higher);
+        }
+
 }
